8a117382927b71acb758bd9e2e216a9b9f7220c7
Vincent Quigley <vinny_182@hotmail.com>
2016-01-29 00:25:03 +0000

    webdriver update true

--- tasks/protractor.js
+++ tasks/protractor.js
@@ -8,6 +8,7 @@ module.exports = function dustjs(grunt) {
       configFile: "./test/client/protractor.conf.js", // Default config file 
       keepAlive: true, // If false, the grunt process stops when the test fails. 
       noColor: false, // If true, protractor will not use colors in its output. 
+      webdriverManagerUpdate: true,
       args: {
         // Arguments passed to the command 
       }
